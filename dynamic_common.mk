8262c4c6f7fb61baa313f8bd7e74d594a4858286
TheKingbuildboot <milmadara65@gmail.com>
2021-08-01 17:12:41 +0000

    Go dotOS

--- dynamic_common.mk
+++ dynamic_common.mk
@@ -124,7 +124,11 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    tcmiface
+    tcmiface \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Netutils
 PRODUCT_PACKAGES += \
